abc24a43aba345e89464c56e3d74808ce7b312fb
Joice Gomes <devjoicegomes@gmail.com>
2023-04-27 00:16:36 -0300

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,64 @@
 const listElement = document.querySelector("[data-list]")
+const loadMoreButton = document.getElementById("btn-load-more")
+
+const generations = {
+  1: {
+    firstPokemonNumber: 1,
+    lastPokemonNumber: 151
+  },
+  2: {
+    firstPokemonNumber: 152,
+    lastPokemonNumber: 251
+  },
+  3: {
+    firstPokemonNumber: 252,
+    lastPokemonNumber: 386
+  },
+  4: {
+    firstPokemonNumber: 387,
+    lastPokemonNumber: 493
+  },
+  5: {
+    firstPokemonNumber: 494,
+    lastPokemonNumber: 649
+  }, 
+}
+
+let chosenGeneration = generations[2]
+
+const limit = 10
+let offset = chosenGeneration.firstPokemonNumber - 1;
 
 const listItemTemplate = (item) => `
-  <li class="item ${item.types[0]}">
-    <span class="number"></span>
-    <span class="name">${item.name}</span>
-    <div class="details">
+  <li class="pokemon ${item.types[0]}">
+    <header class="pokemon__header">
+      <span class="number">${item.id}</span>
+      <span class="name">${item.name}</span>
+    </header>
+    <div class="pokemon__details">
       <ol class="types">
         ${item.types.map(type => `<li class="type">${type}</li>`).join('')}
       </ol>
-      <img src="${item.image}" alt="${item.name}">
+      <img class="image" src="${item.image}" alt="${item.name}">
     </div>
   </li>
 `
-API.getAll().then((list = []) => listElement.innerHTML += list.map(item => listItemTemplate(item)).join(""))
+
+const loadPokemons = (limit, offset) => {
+  API.getAll(limit, offset).then((list = []) => 
+    listElement.innerHTML += list.map(listItemTemplate).join(""))
+}
+
+loadMoreButton.addEventListener('click', (e) => {
+  offset += limit
+  const nextPageTotalItems = offset + limit
+  if (nextPageTotalItems >= chosenGeneration.lastPokemonNumber) {
+    const newLimit = chosenGeneration.lastPokemonNumber - offset
+    loadPokemons(newLimit, offset)
+    e.target.remove()
+  } else {
+    loadPokemons(limit, offset)
+  }
+})
+
+loadPokemons(limit, offset)
--- assets/js/service.js
+++ assets/js/service.js
@@ -1,25 +1,23 @@
 const API = {
-  getDetails: (item) => {
-    return fetch(item.url)
-            .then(response => response.json())
-            .then(createPokemon)
+  getDetails: async (item) => {
+    const response = await fetch(item.url)
+    const json = await response.json()
+    return createPokemon(json)
   },
-  getAll: (limit = 2, offset = 0) => {
+  getAll: async (limit = 2, offset = 0) => {
     const URL = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`;
-    return fetch(URL)
-            .then(response => response.json())
-            .then(json => json.results)
-            .then(list => list.map(item => API.getDetails(item)))
-            .then(promises => Promise.all(promises))
-            .catch(error => console.error(error))
+    const response = await fetch(URL)
+    const json = await response.json()
+    const list = json.results
+    return await Promise.all(list.map(item => API.getDetails(item)))
   }
 }
 
 const createPokemon = (details) => {
-  const pokemon = new Pokemon();
-  pokemon.name = details.name;
-  pokemon.order = details.order;
-  details.types.map(slot => pokemon.types.push(slot.type.name));
-  pokemon.image = details.sprites.other.dream_world.front_default;
-  return pokemon;
+  const pokemon = new Pokemon()
+  pokemon.id = details.id
+  pokemon.name = details.name
+  details.types.map(slot => pokemon.types.push(slot.type.name))
+  pokemon.image = details.sprites.other.dream_world.front_default
+  return pokemon
 }
